97dfdb0940d12f9a3e1dc05ebc4990b9c5143582
username <useremail>
2023-12-22 21:06:56 +0800

    modify:新建一些写法

---
+++ app/src/main/java/cn/demo/myapp/Fragment2.kt
@@ -0,0 +1,40 @@
+package cn.demo.myapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class Fragment2: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // 设置布局
+        val root: View = inflater.inflate(R.layout.f2, container, false)
+
+        // 获取文本组件
+        val textView: TextView = root.findViewById(R.id.textView)
+
+        // 获取 ViewModel
+        var viewModel: MyViewModel = ViewModelProvider(requireActivity(),
+            ViewModelProvider.AndroidViewModelFactory(requireActivity().application))
+            .get(MyViewModel::class.java)
+
+        // 设置文本显示内容
+        textView.setText("${viewModel.progress.value}")
+
+        // 设置 LiveData 监听
+        viewModel.progress.observe(requireActivity(), object : androidx.lifecycle.Observer<Int> {
+            override fun onChanged(t: Int) {
+                textView.setText("${viewModel.progress.value}")
+            }
+        })
+
+        return root
+    }
+}
